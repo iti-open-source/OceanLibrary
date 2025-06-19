77ff045bfda8d6bbdadb0c91829b2e4ab210be2b
Abdelrhaman Elkhouly <khoulyyy@gmail.com>
2025-06-19 04:50:37 +0300

    Add: login page

---
+++ client/src/app/pages/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Renderer2 } from "@angular/core";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrl: "./login.component.css",
+})
+export class LoginComponent implements OnInit {
+  isDarkMode = false;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.isDarkMode = document.body.classList.contains("dark-mode");
+    this.setBodyClass(this.isDarkMode);
+  }
+
+  toggleDarkMode(event: Event) {
+    this.isDarkMode = (event.target as HTMLInputElement).checked;
+    this.setBodyClass(this.isDarkMode);
+  }
+
+  setBodyClass(isDark: boolean) {
+    if (isDark) {
+      this.renderer.addClass(document.body, "dark-mode");
+    } else {
+      this.renderer.removeClass(document.body, "dark-mode");
+    }
+  }
+}
