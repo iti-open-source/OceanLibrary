082f2c6fcddfc33c9ac58f3e34f0d794c1807224
Abdelrhaman Elkhouly <khoulyyy@gmail.com>
2025-06-19 21:46:49 +0300

    Improve UI/UX for mode switcher

--- client/src/app/components/mode-switcher/mode-switcher.component.ts
+++ client/src/app/components/mode-switcher/mode-switcher.component.ts
@@ -1,8 +1,9 @@
 import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-mode-switcher",
-  imports: [],
+  imports: [CommonModule],
   templateUrl: "./mode-switcher.component.html",
   styleUrl: "./mode-switcher.component.css",
 })
