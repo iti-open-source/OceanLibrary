65eedd12bccee0c13636e99d06e586b5d6b2bc53
Adel srour <adelmalys@hotmail.com>
2025-06-19 23:57:36 +0300

    Add orderModel schema

--- server/src/models/orderModel.ts
+++ server/src/models/orderModel.ts
@@ -1,7 +1,54 @@
 import mongoose from "mongoose";
 
+// Item Schema
+const itemSchema = new mongoose.Schema({
+  bookId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "book",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
 // Order schema
-const orderSchema = new mongoose.Schema({});
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  items: {
+    type: [itemSchema],
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: [
+      "pending",
+      "shipped",
+      "delivered",
+      "completed",
+      "cancelled",
+      "rejected",
+    ],
+    default: "pending",
+  },
+});
 
 // Order model
 const orderModel = mongoose.model("orders", orderSchema);
