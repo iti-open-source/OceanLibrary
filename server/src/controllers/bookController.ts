e623e5b5762caebd8a7f08774a72edf0627dcc13
hazemphoenix <hazem.mando408@gmail.com>
2025-06-19 20:57:04 +0300

    feat: add the book creation and fetching in the bookRoutes and implement the createBook and getAllBook with pagination functions

---
+++ server/src/controllers/bookController.ts
@@ -0,0 +1,64 @@
+import { Request, Response, NextFunction } from "express";
+import Book from "../models/bookModel.js";
+import { create } from "domain";
+
+const getAllBooks = async (req: Request, res: Response, next: NextFunction) => {
+  const defaultPageNumber = 1;
+  const defautlLimitNumber = 10;
+  const page = parseInt(req.query.page as string) || defaultPageNumber;
+  const limit = parseInt(req.query.limit as string) || defautlLimitNumber;
+
+  // ex: If we're on page 1 and the limit is 10 -> (1 - 1) * 10 = 0, which is correct we don't wanna skip anything in this case
+  const skip = (page - 1) * limit;
+
+  try {
+    const books = await Book.find().skip(skip).limit(limit);
+    const total = await Book.countDocuments();
+
+    res.json({
+      currentPage: page,
+      totalPages: Math.ceil(total / limit),
+      totalItems: total,
+      data: books,
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ status: "Failure", message: "Something went wrong", error: err });
+  }
+};
+
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const {
+      title,
+      author,
+      genres,
+      price,
+      rating = 0,
+      description,
+      stock,
+      image,
+    } = req.body;
+
+    const book = await Book.insertOne({
+      title,
+      author,
+      genres,
+      price,
+      rating,
+      description,
+      stock,
+      image,
+    });
+    res.status(201).json({
+      status: "Success",
+      message: "Book created successfully",
+      data: book,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default { createBook, getAllBooks };
---
+++ server/src/routes/bookRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import bookController from "../controllers/bookController.js";
+import JoiValidator from "../middlewares/joiValidator.js";
+import { createBookSchema } from "../utils/validation/bookValidation.js";
+
+const router = Router();
+
+// GET /api/books - Get all books
+router.get("/", bookController.getAllBooks);
+
+// POST /api/books - Create new book
+router.post("/", bookController.createBook);
+
+export default router;
