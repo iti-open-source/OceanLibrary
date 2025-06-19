6e27ff529e122a374d8adaf1b781f9060142ad35
Adel srour <adelmalys@hotmail.com>
2025-06-19 21:09:39 +0300

    Added cart, reviews, orders Routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,23 @@
 import express from "express";
 
 import bookRoutes from "./routes/bookRoutes.js";
+import cartRouter from "./routes/cartRoutes.js";
+import reviewRouter from "./routes/reviewRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/books", bookRoutes);
+
+// Apply cart Router
+app.use("/cart", cartRouter);
+
+// Apply review Router
+app.use("/reviews", reviewRouter);
+
+// Apply order Router
+app.use("/orders", orderRouter);
 
 export default app;
