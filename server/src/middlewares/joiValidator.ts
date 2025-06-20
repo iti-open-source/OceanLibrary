f03acbf67a92e747a187c9c8c1cdfe76f5188f60
hazemphoenix <hazem.mando408@gmail.com>
2025-06-20 18:29:00 +0300

    chore: add a bit of documentation to the joiValidator middleware

--- server/src/middlewares/joiValidator.ts
+++ server/src/middlewares/joiValidator.ts
@@ -1,6 +1,12 @@
 import { Schema } from "joi";
 import { Request, Response, NextFunction } from "express";
 
+/**
+ *
+ * @param schema the joi validation schema that you should have created in the utils/validation directory
+ * @returns the middleware that should be applied before trying to access the controller
+ */
+
 const JoiValidator = (schema: Schema) => {
   return (req: Request, res: Response, next: NextFunction): void => {
     const { error } = schema.validate(req.body, { abortEarly: true });
