9b9162ced99f6b4257bd7765db5ca1cedc98e381
Abdelrhaman Elkhouly <khoulyyy@gmail.com>
2025-06-20 20:19:49 +0300

    Add: book hero component

---
+++ client/src/app/components/book-hero/book-hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-hero',
+  imports: [],
+  templateUrl: './book-hero.component.html',
+  styleUrl: './book-hero.component.css'
+})
+export class BookHeroComponent {
+
+}
