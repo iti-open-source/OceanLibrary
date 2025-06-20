4ddface04a24b6216c2794366e32eebbea0b8395
hazemphoenix <hazem.mando408@gmail.com>
2025-06-20 18:35:22 +0300

    chore: update the route paths to follow best practice

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,10 +10,10 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/books", bookRoutes);
-app.use("/api/cart", cartRouter);
-app.use("/api/reviews", reviewRouter);
-app.use("/api/orders", orderRouter);
-app.use("/api/users", userRouter);
+app.use("/api/v1/books", bookRoutes);
+app.use("/api/v1/cart", cartRouter);
+app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/orders", orderRouter);
+app.use("/api/v1/users", userRouter);
 
 export default app;
