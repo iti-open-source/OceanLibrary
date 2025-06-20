e1b5c9c0e8145621354b08ef99018aa92af9c3a0
Abdelrhaman Elkhouly <khoulyyy@gmail.com>
2025-06-20 19:54:33 +0300

    Add: footer component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { ModeSwitcherComponent } from "./components/mode-switcher/mode-switcher.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, ModeSwitcherComponent],
+  imports: [RouterOutlet, ModeSwitcherComponent, FooterComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
