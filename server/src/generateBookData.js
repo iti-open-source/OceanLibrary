53111e33720d9ca6b33961877288c13bfd65bedc
hazemphoenix <hazem.mando408@gmail.com>
2025-06-28 19:41:14 +0300

    chore: create a script to generate a large amount of dummy book data using faker.js

---
+++ server/src/generateBookData.js
@@ -0,0 +1,73 @@
+import Book from "../dist/models/bookModel.js";
+import mongoose from "mongoose";
+import { faker } from "@faker-js/faker";
+import "dotenv/config";
+
+mongoose
+  // eslint-disable-next-line no-undef
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.error("Connection to database failed", error);
+  });
+
+const genresPool = [
+  "fiction",
+  "non-fiction",
+  "fantasy",
+  "science",
+  "history",
+  "romance",
+  "mystery",
+  "biography",
+  "self-help",
+  "horror",
+  "children",
+  "thriller",
+  "business",
+  "philosophy",
+  "comics",
+];
+
+// eslint-disable-next-line no-undef
+
+function getRandomGenres() {
+  const shuffled = [...genresPool].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, Math.floor(Math.random() * 3) + 1); // 1 to 3 genres
+}
+
+function generateBook(id) {
+  return {
+    id,
+    title: faker.book.title(),
+    author: faker.book.author(),
+    genres: getRandomGenres(),
+    price: parseFloat(faker.commerce.price({ min: 5, max: 100 })),
+    ratingAverage: Math.round(Math.random() * 5 * 10) / 10, // 0.0 to 5.0
+    ratingQuantity: faker.number.int({ min: 0, max: 5000 }),
+    description: faker.lorem.paragraphs(1),
+    stock: faker.number.int({ min: 0, max: 100 }),
+    image: faker.image
+      .urlPicsumPhotos({ width: 300, height: 400 })
+      .replace(/blur=[0-9]{1}/g, "blur=2"),
+  };
+}
+
+// Generate N books
+const length = 500;
+const books = Array.from({ length }, (_, index) => generateBook(index + 1));
+
+const populateDatabase = async () => {
+  try {
+    await Book.insertMany(books);
+    console.log("Books collection has been populated!");
+  } catch (error) {
+    console.error("Error populating database:", error);
+  } finally {
+    await mongoose.connection.close();
+  }
+};
+
+populateDatabase();
--- server/src/types/bookFilter.ts
+++ server/src/types/bookFilter.ts
@@ -1,5 +1,6 @@
 export interface BookFilter {
   title?: { $regex: string; $options: string };
+  genres?: { $in: string[] };
   author?: { $regex: string; $options: string };
   price?: { $gte?: number; $lte?: number };
 }
