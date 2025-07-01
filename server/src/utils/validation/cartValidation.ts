50327d1e9989fa41ddf592e52e85e87cd95ae5aa
Adel srour <adelmalys@hotmail.com>
2025-07-01 19:01:59 +0300

    Add validation Schema for Cart Modification

--- server/src/utils/validation/cartValidation.ts
+++ server/src/utils/validation/cartValidation.ts
@@ -8,3 +8,12 @@ export const addToCartSchema = z.object({
     message: "Quantity must be a positive integer",
   }),
 });
+
+export const ModfiyCartSchema = z.object({
+  bookID: z.string().regex(/^[a-f\d]{24}$/i, {
+    message: "Invalid BookID",
+  }),
+  quantity: z.number().int().min(0, {
+    message: "Quantity must be a positive integer",
+  }),
+});
