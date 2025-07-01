3ecedbd86ce2aec8bc52a5105e6bd7b2e94b47cb
Adel srour <adelmalys@hotmail.com>
2025-07-01 18:59:44 +0300

    Implement zod Validation to addToCart Route

--- server/src/routes/cartRoutes.ts
+++ server/src/routes/cartRoutes.ts
@@ -5,11 +5,13 @@ import {
   updateCart,
 } from "../controllers/cartController.js";
 import { verifyToken } from "../middlewares/auth.js";
+import ZodValidator from "../middlewares/zodValidator.js";
+import { addToCartSchema } from "../utils/validation/cartValidation.js";
 
 const router = Router();
 
 router.get("/", verifyToken, viewCart);
-router.post("/", verifyToken, addToCart);
+router.post("/", ZodValidator(addToCartSchema), verifyToken, addToCart);
 router.patch("/", verifyToken, updateCart);
 
 export default router;
