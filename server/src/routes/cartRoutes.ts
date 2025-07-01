26940fc6e359adf6ae4a2481fca471e7310f007a
Adel srour <adelmalys@hotmail.com>
2025-07-01 21:17:31 +0300

    Add DELETE route to clear a single item from the cart

--- server/src/routes/cartRoutes.ts
+++ server/src/routes/cartRoutes.ts
@@ -4,6 +4,7 @@ import {
   addToCart,
   updateCart,
   deleteCart,
+  removeFromCart,
 } from "../controllers/cartController.js";
 import { verifyToken } from "../middlewares/auth.js";
 import ZodValidator from "../middlewares/zodValidator.js";
@@ -18,5 +19,6 @@ router.get("/", verifyToken, viewCart);
 router.post("/", ZodValidator(addToCartSchema), verifyToken, addToCart);
 router.patch("/", ZodValidator(ModfiyCartSchema), verifyToken, updateCart);
 router.delete("/", verifyToken, deleteCart);
+router.delete("/item", verifyToken, removeFromCart);
 
 export default router;
