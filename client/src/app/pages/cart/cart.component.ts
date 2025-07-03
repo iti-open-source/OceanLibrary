18c5fec8a61995a81f6e8a3a2172869c759fc506
Adel srour <adelmalys@hotmail.com>
2025-07-03 22:36:38 +0300

    Add getCart service to fetch cart items

--- client/src/app/pages/cart/cart.component.ts
+++ client/src/app/pages/cart/cart.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CartService } from "../../services/cart.service";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-cart',
-  imports: [],
-  templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  selector: "app-cart",
+  imports: [CommonModule],
+  templateUrl: "./cart.component.html",
+  styleUrl: "./cart.component.css",
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cartItems: [] = [];
 
+  constructor(private cart: CartService) {}
+
+  ngOnInit(): void {
+    this.cart.getCart().subscribe({
+      next: (data) => {
+        this.cartItems = data.userCart.items;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
--- client/src/app/services/cart.service.ts
+++ client/src/app/services/cart.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, tap } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CartService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // Can be used in (navbar) to display number of items in cart
+  cartCount: number = 0;
+
+  /**
+   * Checks if client is a guest or loggedin
+   * @returns boolen
+   */
+  private isGuest() {
+    return true;
+  }
+
+  /**
+   * Get All items in cart
+   * @returns Observable
+   */
+  getCart(): Observable<any> {
+    const clientHeader = this.isGuest()
+      ? ["x-guest-id", "550e8400-e29b-41d4-a716-446655440000"]
+      : [
+          "Authorization",
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODYzZmE1MzlhNGM4NGMwYjk1ZDdiYzAiLCJ1c2VyUm9sZSI6InVzZXIiLCJpYXQiOjE3NTE0MDgzMzksImV4cCI6MTc1MTQxMTkzOX0.VmuXLADNFpXoAGguCMyVdMMyTDUPV8NJz8WIXZJFdl8",
+        ];
+    return this.http
+      .get("http://localhost:3000/api/v1/cart", {
+        headers: {
+          [clientHeader[0]]: clientHeader[1],
+        },
+      })
+      .pipe(
+        tap((data: any) => {
+          this.cartCount = data.items?.length ?? 0;
+        })
+      );
+  }
 }
