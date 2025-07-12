ee6d15dd417232b80e369808e4e2b97dd05a4ad1
Abdelrhaman Elkhouly <khoulyyy@gmail.com>
2025-07-12 13:16:09 +0300

    Add: login, register buttons to navbar

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit, ElementRef } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LucideAngularModule } from "lucide-angular";
 import { RouterLink } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-navbar",
@@ -10,12 +12,42 @@ import { RouterLink } from "@angular/router";
   templateUrl: "./navbar.component.html",
   styleUrl: "./navbar.component.css",
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
   mobileMenuOpen = false;
   profileMenuOpen = false;
   mobileProfileMenuOpen = false;
 
+  constructor(private authService: AuthService, private router: Router, private elRef: ElementRef) {}
+
+  ngOnInit() {
+    document.addEventListener('click', this.handleDocumentClick, true);
+  }
+
+  ngOnDestroy() {
+    document.removeEventListener('click', this.handleDocumentClick, true);
+  }
+
+  handleDocumentClick = (event: MouseEvent) => {
+    if (
+      this.profileMenuOpen &&
+      this.elRef.nativeElement &&
+      !this.elRef.nativeElement.contains(event.target)
+    ) {
+      this.profileMenuOpen = false;
+    }
+  };
+
   toggleMobileMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  signOut() {
+    this.profileMenuOpen = false;
+    this.authService.logout();
+    this.router.navigate(["/login"]);
+  }
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
 }
