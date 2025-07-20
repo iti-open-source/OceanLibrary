5a7ff5b545e53b79a37fa3abcdfb3586c482f5fd
Adel Srour <adelmalys@hotmail.com>
2025-07-21 02:35:14 +0300

    Fix paymob method

--- server/src/utils/payments/paymobSDK.ts
+++ server/src/utils/payments/paymobSDK.ts
@@ -12,58 +12,64 @@ const IFREAME_ID = Number(process.env.PAYMOB_IFRAME_ID!);
  * @returns URL to redirect or embed iframe
  */
 export async function generatePaymobPaymentLink(amountInEGP: number): Promise<Object> {
-    const amountCents = (amountInEGP * 100).toString();
-
-    // 1. Authentication
-    const authRes = await axios.post("https://accept.paymob.com/api/auth/tokens", {
-      api_key: API_KEY,
-    });
-  const token: string = authRes.data.token;
-
-    // 2. Create Order
-    const orderRes = await axios.post("https://accept.paymob.com/api/ecommerce/orders", {
-      auth_token: token,
-      delivery_needed: false,
-      amount_cents: amountCents,
-      currency: "EGP",
-      items: [],
-    });
-  const orderId: number = orderRes.data.id;
+    try {
+      const amountCents = Number((amountInEGP * 100).toFixed(2))
   
-
-    // 3. Get Payment Key
-    const paymentKeyRes = await axios.post(
-      "https://accept.paymob.com/api/acceptance/payment_keys",
-      {
+      // 1. Authentication
+      const authRes = await axios.post("https://accept.paymob.com/api/auth/tokens", {
+        api_key: API_KEY,
+      });
+      const token: string = authRes.data.token;
+      log("Paymob Auth Token:", token);
+  
+      // 2. Create Order
+      const orderRes = await axios.post("https://accept.paymob.com/api/ecommerce/orders", {
         auth_token: token,
+        delivery_needed: false,
         amount_cents: amountCents,
-        expiration: 3600,
-        order_id: orderId,
-        billing_data: {
-          apartment: "NA",
-          email:  "user@example.com",
-          floor: "NA",
-          first_name:  "NA",
-          street: "NA",
-          building: "NA",
-          phone_number: "+201000000000",
-          shipping_method: "NA",
-          postal_code: "NA",
-          city: "NA",
-          country: "NA",
-          last_name: "Doe",
-          state: "NA",
-        },
         currency: "EGP",
-        integration_id: INTEGRATION_ID,
-      }
-    );
-    const paymentToken: string = paymentKeyRes.data.token;
-    
-    // 4. Construct iframe URL
-    const iframeURL = `https://accept.paymob.com/api/acceptance/iframes/${IFREAME_ID}?payment_token=${paymentToken}`;
-
-    return {orderId, iframeURL};
+        items: [],
+      });
+      const orderId: number = orderRes.data.id;
+      
+  
+      // 3. Get Payment Key
+      const paymentKeyRes = await axios.post(
+        "https://accept.paymob.com/api/acceptance/payment_keys",
+        {
+          auth_token: token,
+          amount_cents: amountCents,
+          expiration: 3600,
+          order_id: orderId,
+          billing_data: {
+            apartment: "NA",
+            email:  "user@example.com",
+            floor: "NA",
+            first_name:  "NA",
+            street: "NA",
+            building: "NA",
+            phone_number: "+201000000000",
+            shipping_method: "NA",
+            postal_code: "NA",
+            city: "NA",
+            country: "NA",
+            last_name: "NA",
+            state: "NA",
+          },
+          currency: "EGP",
+          integration_id: INTEGRATION_ID,
+        }
+      );
+      const paymentToken: string = paymentKeyRes.data.token;
+      
+      // 4. Construct iframe URL
+      const iframeURL = `https://accept.paymob.com/api/acceptance/iframes/${IFREAME_ID}?payment_token=${paymentToken}`;
+  
+      return {orderId, iframeURL};
+    } catch (error) {
+      console.log("Error generating Paymob payment link:", error);
+      throw new Error("Failed to generate payment link paymob is currently down");
+    }
 }
 
 /**
